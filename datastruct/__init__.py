a567b6293b56952756d1432a54fd799dff20c74e
Hernan <hernan.grecco@gmail.com>
2022-01-22 09:45:14 -0300

    Add test funcionts in testsuite module and certain packaging

--- datastruct/__init__.py
+++ datastruct/__init__.py
@@ -18,7 +18,22 @@ from .exceptions import (
     WrongValueError,
 )
 
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Backport for Python < 3.8
+    from importlib_metadata import version
+
+try:  # pragma: no cover
+    __version__ = version("datastruct")
+except Exception:  # pragma: no cover
+    # we seem to have a local copy not installed without setuptools
+    # so the reported version will be unknown
+    __version__ = "unknown"
+
+
 __all__ = [
+    __version__,
     validators,
     DataStruct,
     MissingValueError,
